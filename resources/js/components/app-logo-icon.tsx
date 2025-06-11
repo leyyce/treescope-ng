3f9441c840f1b9883565d258bd3c48038ba0d8e4
Leya Wehner <leya.wehner@proton.me>
2025-06-11 23:30:16 +0200

    Changed dashboard branding

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -2,12 +2,32 @@ import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
     return (
-        <svg {...props} viewBox="0 0 40 42" xmlns="http://www.w3.org/2000/svg">
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M17.2 5.63325L8.6 0.855469L0 5.63325V32.1434L16.2 41.1434L32.4 32.1434V23.699L40 19.4767V9.85547L31.4 5.07769L22.8 9.85547V18.2999L17.2 21.411V5.63325ZM38 18.2999L32.4 21.411V15.2545L38 12.1434V18.2999ZM36.9409 10.4439L31.4 13.5221L25.8591 10.4439L31.4 7.36561L36.9409 10.4439ZM24.8 18.2999V12.1434L30.4 15.2545V21.411L24.8 18.2999ZM23.8 20.0323L29.3409 23.1105L16.2 30.411L10.6591 27.3328L23.8 20.0323ZM7.6 27.9212L15.2 32.1434V38.2999L2 30.9666V7.92116L7.6 11.0323V27.9212ZM8.6 9.29991L3.05913 6.22165L8.6 3.14339L14.1409 6.22165L8.6 9.29991ZM30.4 24.8101L17.2 32.1434V38.2999L30.4 30.9666V24.8101ZM9.6 11.0323L15.2 7.92117V22.5221L9.6 25.6333V11.0323Z"
-            />
+        <svg {...props} viewBox="0 0 416 561" width="416" height="561" xmlns="http://www.w3.org/2000/svg">
+
+            <g>
+                <title>Layer 1</title>
+                <rect id="svg_1" height="289" width="86" y="272" x="167" stroke="#000" fill="#7f4f02"/>
+                <ellipse ry="99" rx="94.5" id="svg_2" cy="218" cx="94.5" stroke="#000" fill="#7fff00"/>
+                <ellipse ry="144" rx="91.5" id="svg_3" cy="144" cx="160.5" stroke="#000" fill="#7fff00"/>
+                <ellipse ry="144" rx="91.5" id="svg_4" cy="144" cx="160.5" stroke="#000" fill="#7fff00"/>
+                <ellipse ry="99" rx="94.5" id="svg_7" cy="230" cx="321.5" stroke="#000" fill="#7fff00"/>
+                <ellipse ry="144" rx="91.5" id="svg_6" cy="149" cx="246.5" stroke="#000" fill="#7fff00"/>
+                <ellipse ry="23.5" rx="20.5" id="svg_9" cy="116.5" cx="234.5" stroke="#000" fill="#ff0000"/>
+                <ellipse ry="23.5" rx="20.5" id="svg_10" cy="170.5" cx="120.5" stroke="#000" fill="#ff0000"/>
+                <ellipse ry="23.5" rx="20.5" id="svg_11" cy="94.5" cx="132.5" stroke="#000" fill="#ff0000"/>
+                <ellipse ry="23.5" rx="20.5" id="svg_12" cy="193.5" cx="261.5" stroke="#000" fill="#ff0000"/>
+                <ellipse ry="23.5" rx="20.5" id="svg_13" cy="223.5" cx="373.5" stroke="#000" fill="#ff0000"/>
+                <ellipse ry="23.5" rx="20.5" id="svg_14" cy="278.5" cx="72.5" stroke="#000" fill="#ff0000"/>
+                <ellipse ry="35.5" rx="20" id="svg_15" cy="379.5" cx="213" stroke="#000" fill="#191919"/>
+                <ellipse ry="16" rx="9.5" id="svg_16" cy="400" cx="211.5" stroke="#000" fill="#7f7f7f"/>
+                <ellipse ry="8.5" rx="7" id="svg_18" cy="379.5" cx="210" stroke="#000" fill="#7f7f7f"/>
+                <line id="svg_25" y2="384.63727" x2="210.81373" y1="381.10785" x1="208.06863" stroke="#ffaa56" fill="none"/>
+                <line id="svg_26" y2="379.93138" x2="212.38236" y1="383.85295" x1="210.42157" stroke="#ffaa56" fill="none"/>
+                <ellipse ry="1.56863" rx="0.98039" id="svg_29" cy="377.18629" cx="206.30393" fill="#191919"/>
+                <ellipse ry="1.76471" rx="1.37255" id="svg_30" cy="376.20589" cx="210.61765" fill="#191919"/>
+                <ellipse stroke="#000" ry="9.80392" rx="4.11765" id="svg_32" cy="399.93138" cx="202.38236" fill="#7f7f7f"/>
+                <ellipse ry="8.43137" rx="3.72549" id="svg_33" cy="399.73531" cx="220.81373" stroke="#000" fill="#7f7f7f"/>
+            </g>
         </svg>
     );
 }
